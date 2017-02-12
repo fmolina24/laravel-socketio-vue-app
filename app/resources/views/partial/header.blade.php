64e9eab73ca11e0d0b3dc343a578f82997faa921
Frank Molina <frank@weareenvoy.com>
2017-02-11 18:27:24 -0800

    Fixing routes.

--- app/resources/views/partial/header.blade.php
+++ app/resources/views/partial/header.blade.php
@@ -1,6 +1,6 @@
 <nav>
     <div class="nav-wrapper blue-grey darken-2">
-        <a href="#" class="brand-logo">Examples</a>
+        <a href="/" class="brand-logo">Examples</a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
             <li><a href="chat">Chat</a></li>
             <li><a href="realtime">Real Time Data</a></li>
