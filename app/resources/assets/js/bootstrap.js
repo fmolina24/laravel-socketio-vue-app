6a4554aa3614856b4bdf2675b8ceeefea9663aad
Frank Molina <frank@weareenvoy.com>
2017-02-11 18:24:05 -0800

    Adding some templates for example pages.

--- app/resources/assets/js/bootstrap.js
+++ app/resources/assets/js/bootstrap.js
@@ -1,31 +1,18 @@
+import _ from "lodash";
+window._ = _;
 
-window._ = require('lodash');
+import "materialize-css";
 
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-window.$ = window.jQuery = require('jquery');
-
-require('bootstrap-sass');
-
-/**
- * Vue is a modern JavaScript library for building interactive web interfaces
- * using reactive data binding and reusable components. Vue's API is clean
- * and simple, leaving you to focus on building your next great project.
- */
-
-window.Vue = require('vue');
+import Vue from "vue";
+window.Vue = Vue;
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-
-window.axios = require('axios');
+import axios from "axios";
+window.axios = axios;
 
 window.axios.defaults.headers.common = {
     'X-CSRF-TOKEN': window.Laravel.csrfToken,
